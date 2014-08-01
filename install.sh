7e7b5d67137d910175ae1808c944611585e87d3a
mookle <mwaddilove@gmail.com>
2014-08-02 00:53:36 +0100

    Utility script to install the dotfiles

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Transfer the dotfiles to ~/ and set some variables
+#
+
+br="\n----------\n"
+
+echo "WARNING! This script will overwrite any existing dotfiles."
+while true; do
+    read -p "Do you wish to continue? [Y/n]: " yn
+    case ${yn:-Y} in
+        [yYes]* ) break;;
+        [nNo]* ) exit;;
+        * ) echo "Please answer y(es) or n(o)";;
+    esac
+done
+
+echo -e $br
+
+rsync --exclude ".git/" --exclude "README.md" --exclude "install.sh" \
+      --exclude ".DS_Store" -av --no-perms . ~
+
+echo -e $br
+
+read -p ".gitconfig needs to set user.email: " email
+sed -i .bk s/\<EMAIL\>/"$email"/g ~/.gitconfig; rm ~/.gitconfig.bk
+read -p ".vimrc needs a name for the phpDoc @author tag: " name
+sed -i .bk s/\<AUTHOR\>/"$name"/g ~/.vimrc; rm ~/.vimrc.bk
+
+echo -e "\nAll done!\n"
