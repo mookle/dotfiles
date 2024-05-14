7595503d046d639dad47fdfa3717ca59fe10b3a2
mookle <aaf4c@waddi.love>
2024-05-14 12:17:38 +0100

    Add mappings to build/run a Godot scene [neovim]

--- .config/nvim/lua/plugins/vim-godot.lua
+++ .config/nvim/lua/plugins/vim-godot.lua
@@ -9,5 +9,8 @@ return {
             cmd = { "nc", "localhost", "6005" },
             capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
         }
+
+        require("utils").map("n", "<space>r", "<cmd>GodotRun<cr>")
+        require("utils").map("n", "<space>c", "<cmd>GodotRunCurrent<cr>")
     end,
 }
