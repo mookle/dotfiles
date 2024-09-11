f146d151f246a6711ec41256f8e59c174a340570
mookle <aaf4c@waddi.love>
2024-09-11 17:32:12 +0100

    Add substitute operator [neovim]

---
+++ .config/nvim/lua/plugins/substitute.lua
@@ -0,0 +1,13 @@
+return {
+    "gbprod/substitute.nvim",
+    config = function(_, opts)
+        local substitute = require("substitute")
+        local map = require("utils").map
+        local mopts = { noremap = true }
+        substitute.setup(opts)
+        map("n", "s", substitute.operator, mopts)
+        map("n", "ss", substitute.line, mopts)
+        map("n", "S", substitute.eol, mopts)
+        map("x", "s", substitute.visual, mopts)
+    end,
+}
