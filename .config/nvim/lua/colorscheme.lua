b0363b93640b509eb2023c5cc77fe84f25083bbf
mookle <aaf4c@waddi.love>
2024-09-21 22:31:16 +0100

    A few more colorschemes, why not [neovim]

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,6 +1,9 @@
 return {
     'mookle/autobot-vim',
     'jacoborus/tender.vim',
+    'sainnhe/sonokai',
+    'sainnhe/gruvbox-material',
+    'morhetz/gruvbox',
     {
         "mcchrish/zenbones.nvim",
         dependencies = {
@@ -39,5 +42,23 @@ return {
                 disable_italics = true,
             })
         end,
+    },
+    {
+        'shaunsingh/nord.nvim',
+        init = function()
+            vim.g.nord_contrast = true
+            vim.g.nord_borders = false
+            vim.g.nord_disable_background = false
+            vim.g.nord_italic = false
+            vim.g.nord_uniform_diff_background = true
+            vim.g.nord_bold = true
+        end,
+    },
+    {
+        'sainnhe/everforest',
+        init = function()
+            vim.g.everforest_background = 'hard'
+            vim.g.everforest_enable_italic = true
+        end
     },
 }
