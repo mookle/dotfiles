a27424ee6b9ee5ee9dbea328863612e00545691e
mookle <aaf4c@waddi.love>
2024-09-26 16:08:23 +0100

    Update lazy.nvim bootstrap [neovim]

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,27 +1,26 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("options")
+
 require("lazy").setup({
-    {import = "plugins"},
-    {import = "colorscheme"},
+    spec = {
+        { import = "plugins" },
+        { import = "colorscheme" },
+    },
+    checker = { enabled = false },
 })
-
---vim.cmd.cabbr({ args = { "<expr>", "%", "&filetype == 'oil' ? bufname('%')[6:] : '%'" } })
-
---local augroup_cdpwd = vim.api.nvim_create_augroup('augroup_cdpwd', { clear = true })
---vim.api.nvim_create_autocmd("VimEnter", {
---    pattern = '*',
---    group = augroup_cdpwd,
---  callback = function() vim.api.nvim_set_current_dir(vim.cmd.expand('%')) end,
---})
