7611b1332c5e9d5535fa6945bf0ed7b0281e7425
mookle <aaf4c@waddi.love>
2024-10-16 22:14:34 +0100

    Prompt for title when adding new ZK note [neovim]

--- .config/nvim/lua/plugins/zettelkasten.lua
+++ .config/nvim/lua/plugins/zettelkasten.lua
@@ -1,4 +1,16 @@
 local zk_dir = vim.fn.expand '~' .. '/zettelkasten'
+
+local title_prompt = function()
+    vim.ui.input({ prompt = 'Enter note title: ' }, function(input)
+        if input ~= nil then
+            require('zk.api').new(zk_dir, { dir = 'unfiled', title = input }, function(err, res)
+                vim.cmd('e ' .. res.path)
+                vim.api.nvim_feedkeys('Gi', 't', false)
+            end)
+        end
+    end)
+end
+
 return {
     {
         "zk-org/zk-nvim",
@@ -10,7 +22,7 @@ return {
           'BufNewFile ' .. zk_dir .. '/*.md',
         },
         keys = {
-            { '<leader>n', '<cmd>ZkNew { dir = "unfiled" }<cr>' },
+            { '<leader>n', title_prompt },
         }
     },
 }
