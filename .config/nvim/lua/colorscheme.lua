f772e03f0b6426931f59b9513548ccb822e4e2f0
mookle <aaf4c@waddi.love>
2024-10-21 08:54:03 +0100

    More theming nonsense

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -8,34 +8,32 @@ return {
                     'nord',
                     'nordbones',
                     'rosebones',
+                    'rose-pine-moon',
+                    'oceanic_material',
+                    'OceanicNext',
                     'nightfox',
                     'noctis_minimus',
+                    'kanagawa',
+                    'kanagawa-paper',
                     'forestbones',
                     'terafox',
                     'neobones',
-                    'kanagawa-paper',
-                    'gruvbox-baby',
-                    'zenbones',
+                    'two-firewatch',
+                    'pink-moon',
+                    'neofusion',
                 }
             })
         end,
     },
-    {
-        'sho-87/kanagawa-paper.nvim',
-        lazy = true,
-    },
-    {
-        'luisiacc/gruvbox-baby',
-        lazy = true,
-    },
-    {
-        'EdenEast/nightfox.nvim',
-        lazy = true,
-    },
-    {
-        'talha-akram/noctis.nvim',
-        lazy = true,
-    },
+    { 'sts10/vim-pink-moon', lazy = true },
+    { 'nvimdev/oceanic-material', lazy = true },
+    { 'mhartington/oceanic-next', lazy = true },
+    { 'rakr/vim-two-firewatch', lazy = true },
+    { 'sho-87/kanagawa-paper.nvim', lazy = true },
+    { 'EdenEast/nightfox.nvim', lazy = true },
+    { 'talha-akram/noctis.nvim', lazy = true },
+    { 'rebelot/kanagawa.nvim', lazy = true },
+    { 'rose-pine/neovim', lazy = true },
     {
         'mcchrish/zenbones.nvim',
         dependencies = {
@@ -58,11 +56,19 @@ return {
         init = function()
             vim.g.nord_contrast = true
             vim.g.nord_borders = true
-            vim.g.nord_disable_background = false
-            vim.g.nord_italic = false
+            vim.g.nord_disable_background = true
+            vim.g.nord_italic = true
             vim.g.nord_uniform_diff_background = true
-            vim.g.nord_bold = false
+            vim.g.nord_bold = true
         end,
         lazy = true,
+    },
+    {
+        'diegoulloao/neofusion.nvim',
+        opts = {
+            terminal_colors = false,
+            dim_inactive = true,
+            bold = false,
+        },
     },
 }
