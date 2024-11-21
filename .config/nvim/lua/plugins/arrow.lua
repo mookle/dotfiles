832d0c2abe5e92de9df26d01defd4381c2faad1b
mookle <aaf4c@waddi.love>
2024-11-21 17:04:00 +0000

    Remove labels that clash w/arrow's default mappings [neovim]

--- .config/nvim/lua/plugins/arrow.lua
+++ .config/nvim/lua/plugins/arrow.lua
@@ -1,12 +1,11 @@
 -- merge this, telescope, and oil into a file-system/navigation spec?
-local labels = 'tnseriaogmplfuwyqbjdhdcxzkv'
+local labels = 'tnriaogmplfuwybjhczk'
 
 return {
     'otavioschwanck/arrow.nvim',
     opts = {
         show_icons = false,
-        leader_key = ';', -- Recommended to be a single key
-        -- buffer_leader_key = 'm', -- Per Buffer Mappings
+        leader_key = ';',
         index_keys = labels,
     },
     keys = {
