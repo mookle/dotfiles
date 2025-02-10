99e729009dac797e7890eff01b00c60766c3074f
mookle <aaf4c@waddi.love>
2025-02-10 02:39:57 +0000

    Remove ToggleTerm plugin [neovim]

--- .config/nvim/lua/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- Toggleable terminal buffer(s) that persist throughout a session
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = true,
-    cmd = 'ToggleTerm',
-}
-
