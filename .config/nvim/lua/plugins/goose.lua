cb28e622d8f4a7eae665e13d86de1f7f2673ea9f
mookle <aaf4c@waddi.love>
2025-06-03 21:06:25 +0100

    Add Goose (AI) plugin to Neovim

---
+++ .config/nvim/lua/plugins/goose.lua
@@ -0,0 +1,16 @@
+return {
+    "azorng/goose.nvim",
+    opts = {},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "MeanderingProgrammer/render-markdown.nvim",
+            opts = {
+                anti_conceal = { enabled = false },
+            },
+        }
+    },
+    ft = {
+        'elixir', 'go', 'rust'
+    }
+}
